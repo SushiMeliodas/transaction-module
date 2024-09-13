6ef935fd238b4d3e02e86d6251a814bcd4eabcf5
bryan_sim <bryanyf@hotmail.com>
2024-09-13 22:35:18 +0800

    added new assets & components

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,16 +1,29 @@
-import ActionButton from "@/components/ActionButton";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import { authenticateAsync } from "expo-local-authentication";
+
+import ActionButton from "@/components/ActionButton";
 
 const Login = () => {
-  const onLoginPress = () => {};
+  const onLoginPress = async () => {
+    const bioRes = await authenticateAsync();
+
+    console.warn(bioRes);
+
+    // router.replace("/(root)/(tabs)/history");
+  };
 
   return (
-    <SafeAreaView className="bg-white">
-      <View className="flex flex-col items-center justify-center h-full p-5 gap-8 box-border">
-        <Text>Login to view your Transaction history.</Text>
+    <SafeAreaView className="flex-1 bg-white  p-5">
+      <View className="flex-col justify-center items-center h-full">
+        <Text className="text-lg">Login to view your Transaction history</Text>
 
-        <ActionButton title="Login" onPress={onLoginPress} />
+        <ActionButton
+          title="Login"
+          onPress={onLoginPress}
+          className="w-10/12 mt-12"
+        />
       </View>
     </SafeAreaView>
   );
